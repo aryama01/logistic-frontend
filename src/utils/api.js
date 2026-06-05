a77e1b5456fcb3914ed2341b6dee6881aa0357d4
aryama01 <aryamasham@gmail.com>
2026-06-05 11:15:40 +0530

    render url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = import.meta.env.VITE_API_URL || '/api'
+const API = 'https://logistic-backend-umhs.onrender.com' || '/api'
 
 export const api = {
   // Locations
